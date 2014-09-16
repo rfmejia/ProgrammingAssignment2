4fbf9ba22c19849d27f75599c8751d90db529483
Akie Mejia <locker47@gmail.com>
2014-09-16 16:41:08 +0900

    Added makeCacheMatrix and cacheSolve functions with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+# A function wrapper to a matrix that caches its the computation of its inverse.
+makeCacheMatrix <- function(mat = matrix()) {
+  # Cached computation of inverse
+  inv <- NULL
 
-## Write a short comment describing this function
+  get <- function() mat
+  get.inverse <- function() inv  
+  
+  # Changes the supplied matrix and resets the computation value in the
+  # parent environment
+  set <- function(newMat) {
+    mat <<- newMat
+    inv <<- NULL
+  }
 
-makeCacheMatrix <- function(x = matrix()) {
+  # Set the computation value in the parent environment
+  set.inverse <- function(newInv) inv <<- newInv
 
+  list(
+    get = get,
+    get.inverse = get.inverse,
+    set = set, 
+    set.inverse = set.inverse
+  )
 }
 
-
-## Write a short comment describing this function
-
+# Given an instance of the makeCacheMatrix() function wrapper and *optional* 
+# argument(s), execute the solve() function on the cached matrix with the 
+# supplied argument(s) to compute for its inverse and cache into the instance
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$get.inverse()
+  
+  # Return the cached inverse computation, if it exists
+  if(!is.null(inv)) inv
+  else { # If it does not exist, compute the inverse and cache before returning
+    mat <- x$get()
+    inv <- solve(mat, ...) # Pass the supplied optional arguments
+    x$set.inverse(inv)
+    inv
+  }
 }
